ff51b291cdbcdafccc3c900be88a317165d841c5
kanhataak97@gmail.com <kanhataak97@gmial.com>
2021-11-24 13:46:48 +0530

    udataed wafer project

--- main.py
+++ main.py
@@ -94,10 +94,10 @@ def trainRouteClient():
         return Response("Error Occurred! %s" % e)
     return Response("Training successful!!")
 
-# port = int(os.getenv("PORT",5000))
+port = int(os.getenv("PORT",5000))
 if __name__ == "__main__":
     host = '0.0.0.0'
-    port = 5000
+    # port = 5000
     httpd = simple_server.make_server(host, port, app)
     # print("Serving on %s %d" % (host, port))
     httpd.serve_forever()
